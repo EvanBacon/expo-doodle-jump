aef2694add2b65fc7dbb8285cbfc8ba330d49ecd
Evan Bacon <baconbrix@gmail.com>
2019-04-22 19:21:52 -0700

    Added expo log

--- ExpoButton.js
+++ ExpoButton.js
@@ -1,6 +1,38 @@
 import React from 'react';
 import { Image, Text, TouchableOpacity, View } from 'react-native';
-import { Linking } from 'expo';
+import { Linking, Constants } from 'expo';
+
+let saidHello = false;
+
+// From PIXI.js
+function sayHello(type) {
+  if (saidHello) {
+    return;
+  }
+
+  if (navigator.userAgent.toLowerCase().indexOf('chrome') > -1) {
+    console.log(
+      '\n %c %c %c Expo Web ' +
+        Constants.expoVersion +
+        ' %c  %c  www.expo.io  %c %c \n\n',
+      'background: #4630EB; padding:5px 0;',
+      'background: #4630EB; padding:5px 0;',
+      'color: #ffffff; background: #030307; padding:5px 0;',
+      'background: #4630EB; padding:5px 0;',
+      'background: #4630EB; padding:5px 0;',
+      'background: #4630EB; padding:5px 0;',
+      'color: #4630EB; background: #fff; padding:5px 0;',
+    );
+  } else if (window.console) {
+    console.log(
+      'Expo Web ' + Constants.expoVersion + ' - ' + type + ' - www.expo.io',
+    );
+  }
+
+  saidHello = true;
+}
+
+sayHello('Bacon');
 
 const ExpoButton = () => (
   <TouchableOpacity
